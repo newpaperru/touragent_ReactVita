df30357a56ce5dd89de526704e36e2a7fd5a2cff
Борис Архангельский <boryanski213@gmail.com>
2025-05-19 17:33:23 +0300

    Исправлена недоработка с анимацией HeaderHostelRoom. Теперь при скроллинге он вновь фиксируется вверху окна пользователя.

--- src/components/HostelRoomPage/HostelRoom_elements/HeaderHostelRoom.jsx
+++ src/components/HostelRoomPage/HostelRoom_elements/HeaderHostelRoom.jsx
@@ -1,11 +1,13 @@
 import styles from "../HosteRoom.module.css";
 import { useBookingData } from "../customHooks/useBookingData";
 
-export const HeaderHostelRoom = ({ header_data }) => {
+export const HeaderHostelRoom = ({ header_data, isFixed }) => {
     const { dateRange, totalGuests} = useBookingData();
 
     return (
-        <div className={styles.header}>
+        <div className={`${styles.header} ${
+            isFixed ? `${styles.fixed} ${styles.visible}` : ''
+        }`}>
             <span className={styles.name_hostel}>{header_data}</span>
             <div className={styles.info_user_chose}>
                 <span className={styles.user_date}>{dateRange}</span>
