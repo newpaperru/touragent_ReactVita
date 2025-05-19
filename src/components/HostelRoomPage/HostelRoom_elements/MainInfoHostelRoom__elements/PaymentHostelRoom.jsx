09f4a1976fafda9151e50d001dbb0a2af8f22c74
Борис Архангельский <boryanski213@gmail.com>
2025-05-19 17:12:39 +0300

    Теперь ценник высвечивается с учетом количества гостей. Так же теперь месяца отображаются в английской версии после добавления параметра locale.

--- src/components/HostelRoomPage/HostelRoom_elements/MainInfoHostelRoom__elements/PaymentHostelRoom.jsx
+++ src/components/HostelRoomPage/HostelRoom_elements/MainInfoHostelRoom__elements/PaymentHostelRoom.jsx
@@ -1,9 +1,15 @@
+import { useBookingData } from "../../customHooks/useBookingData";
 import styles from "../../HosteRoom.module.css";
 
 export const PaymentHostelRoom = ({ cost, breakfast, payment }) => {
+    const { totalGuests } = useBookingData();
+    const totalCost = cost * totalGuests;
+    
     return (
         <div className={styles.payment_hostel_room}>
-            <span className={styles.cost}>{cost}$</span>
+            <span className={styles.cost}>
+                {totalCost}$
+            </span>
             {breakfast === true ? (
                 <span className={`${styles.breakfast} ${styles.active}`}>
                     Breakfast is included
